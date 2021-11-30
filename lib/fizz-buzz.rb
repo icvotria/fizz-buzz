36e431f6e0564fb80401dfe01ba1dbe92ef0cde9
Victoria <icvotria@gmail.com>
2021-11-30 11:55:55 +0000

    checked for numbers 1 to 100

--- lib/fizz-buzz.rb
+++ lib/fizz-buzz.rb
@@ -4,7 +4,10 @@ def fizz_buzz(n)
   answer << "Buzz" if n % 5 == 0
   answer.length == 0 ? n : answer
 end
-
+# for x in 1..100 do 
+#   puts fizz_buzz(x)
+# end
+(1..100).map { |x| puts fizz_buzz(x) }
 =begin
 def fizz_buzz(n)
   if n % 15 == 0
