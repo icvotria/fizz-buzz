56345c61435f52be8b093783d9be72bb035790c8
Victoria <icvotria@gmail.com>
2021-11-30 11:34:46 +0000

    added tests for 15 and 7

--- spec/fizz-buzz_spec.rb
+++ spec/fizz-buzz_spec.rb
@@ -6,4 +6,10 @@ describe 'FizzBuzz' do
   it "returns 'Buzz' when passed 5" do
     expect(fizz_buzz(5)).to eq 'Buzz'
   end
+  it "returns 'FizzBuzz' when passed 15" do
+    expect(fizz_buzz(15)).to eq 'FizzBuzz'
+  end
+  it "returns 7 when passed 7" do
+    expect(fizz_buzz(7)).to eq 7
+  end
 end
