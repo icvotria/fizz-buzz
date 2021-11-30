1681b7a1322993b8bc5f795d76751bedd8f91f24
SophieGowen <sophie.j.gowen@gmail.com>
2021-11-30 11:43:35 +0000

    refactor

--- lib/fizz-buzz.rb
+++ lib/fizz-buzz.rb
@@ -1,4 +1,12 @@
 def fizz_buzz(n)
+  answer = ""
+  answer = "Fizz" if n % 3 == 0
+  answer << "Buzz" if n % 5 == 0
+  answer.length == 0 ? n : answer
+end
+
+=begin
+def fizz_buzz(n)
   if n % 15 == 0
     "FizzBuzz"
   elsif n % 3 == 0
@@ -9,3 +17,4 @@ def fizz_buzz(n)
     n
   end
 end
+=end
