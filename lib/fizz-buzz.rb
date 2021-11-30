5468f03ea0d6c7747280ca54f36367e3d66cd2a7
Victoria <icvotria@gmail.com>
2021-11-30 11:15:08 +0000

    first test

--- lib/fizz-buzz.rb
+++ lib/fizz-buzz.rb
@@ -1,7 +1,6 @@
 def fizz_buzz(n)
   if n % 3 == 0
-    return "Fizz"
+    "Fizz"
   else n
   end
 end
-p fizz_buzz(3)
---
+++ spec/fizz-buzz_spec.rb
@@ -0,0 +1,6 @@
+require 'fizz-buzz'
+describe 'FizzBuzz' do
+  it "returns 'Fizz' when passed 3" do
+    expect(fizz_buzz(3)).to eq 'Fizz'
+  end
+end
